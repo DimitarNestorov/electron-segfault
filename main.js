e7cfb32d8f714f31a8ec60641147cd9295547fe8
Dimitar Nestorov <mitkones@gmail.com>
2018-07-18 21:41:05 +0300

    segfault

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 
+require('@sentry/electron').init({dsn: 'https://45528e0155754fe184d09f916051fe3f@sentry.io/1', release: '123'});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
